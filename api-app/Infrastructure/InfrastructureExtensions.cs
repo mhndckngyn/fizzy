d406f9598c87a7278e05bc420044f3be74b90d59
ducminh <126580562+mhndckngyn@users.noreply.github.com>
2026-03-07 19:02:29 +0700

    Initialize Infrastructure project

---
+++ api-app/Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,31 @@
+using Infrastructure.Database;
+using Infrastructure.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddSingleton<AuditableEntitySaveChangesInterceptor>();
+        services.AddDbContext<AppDbContext>(
+            (sp, options) =>
+            {
+                options.UseNpgsql(connectionString);
+                options.AddInterceptors(
+                    sp.GetRequiredService<AuditableEntitySaveChangesInterceptor>()
+                );
+            }
+        );
+
+        return services;
+    }
+}
