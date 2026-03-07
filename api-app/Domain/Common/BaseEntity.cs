8fbad106dadfd30247e60a514923ef71c9fa9f53
ducminh <126580562+mhndckngyn@users.noreply.github.com>
2026-03-07 18:56:17 +0700

    Initialize Domain project

---
+++ api-app/Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Common;
+
+public abstract class BaseEntity : IAuditableEntity
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
+}
