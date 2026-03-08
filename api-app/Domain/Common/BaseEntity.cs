97b48035124ad63b4e17708d3f25996204a22d49
ducminh <126580562+mhndckngyn@users.noreply.github.com>
2026-03-08 22:42:14 +0700

    Add default value for Entity Id

--- api-app/Domain/Common/BaseEntity.cs
+++ api-app/Domain/Common/BaseEntity.cs
@@ -2,7 +2,7 @@
 
 public abstract class BaseEntity : IAuditableEntity
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
 
     public DateTime CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
