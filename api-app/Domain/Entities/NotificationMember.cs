a0c040dfddf3a2ef6f49185ebce347265c2ca8a8
anhkhoa13-dev <itcsiu22278@student.hcmiu.edu.vn>
2026-03-16 23:44:59 +0700

    feat: Implement notification system with entities, configurations, and endpoints

---
+++ api-app/Domain/Entities/NotificationMember.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class NotificationMember : BaseEntity
+{
+    public Guid NotificationId { get; }
+    public Guid RecepientMemberId { get; }
+    public bool IsRead { get; private set; }
+
+    public void MarkAsRead()
+    {
+        IsRead = true;
+    }
+
+    public void MarkAsUnread()
+    {
+        IsRead = false;
+    }
+}
