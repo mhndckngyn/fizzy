0930cf73c4985bf72cb238265224704045a90bf4
Minh Nguyen Duc <126580562+mhndckngyn@users.noreply.github.com>
2026-03-29 18:05:10 +0700

    fix(mobile): resolve mobile flicker on navigation (#19)

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -6,12 +6,44 @@ import {
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import { TamaguiProvider } from "tamagui";
+import { TamaguiProvider, useTheme, useThemeName } from "tamagui";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { queryClient } from "@/lib/query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { tamaguiConfig } from "../tamagui.config";
+
+function RootLayoutNav() {
+  const theme = useTheme();
+  const themeName = useThemeName();
+  const isDark = themeName.startsWith("dark");
+
+  return (
+    <ThemeProvider
+      value={
+        isDark
+          ? {
+              ...DarkTheme,
+              colors: { ...DarkTheme.colors, background: theme.background.val },
+            }
+          : {
+              ...DefaultTheme,
+              colors: {
+                ...DefaultTheme.colors,
+                background: theme.background.val,
+              },
+            }
+      }
+    >
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="sign-in" />
+        <Stack.Screen name="(workspace)" />
+      </Stack>
+
+      <StatusBar style={isDark ? "light" : "dark"} />
+    </ThemeProvider>
+  );
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -20,19 +52,9 @@ export default function RootLayout() {
     <QueryClientProvider client={queryClient}>
       <TamaguiProvider
         config={tamaguiConfig}
-        // defaultTheme={colorScheme === "dark" ? "dark" : "light"}
-        defaultTheme={"light"}
+        defaultTheme={colorScheme === "dark" ? "dark" : "light"}
       >
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="sign-in" />
-            <Stack.Screen name="(workspace)" />
-          </Stack>
-
-          <StatusBar style="auto" />
-        </ThemeProvider>
+        <RootLayoutNav />
       </TamaguiProvider>
     </QueryClientProvider>
   );
