0d4b8e68d59d93f08fc66bfda6bc3dcafe1b118f
Minh Nguyen Duc <126580562+mhndckngyn@users.noreply.github.com>
2026-08-12 12:02:32 +0700

    user: migrations and config for soft delete

--- api-app/Domain/Entities/User.cs
+++ api-app/Domain/Entities/User.cs
@@ -5,4 +5,8 @@ namespace Domain.Entities;
 public class User : BaseEntity
 {
     public required string EmailAddress { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public ICollection<Member> Members { get; set; } = new List<Member>();
 }
